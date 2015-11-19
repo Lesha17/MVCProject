b493a2f9cd795b9392224532f1dec6f3436352e8
Lesha17 <17Aleshka@gmail.com>
2015-11-19 17:12:33 +0300

    Working Preview

--- WebApiServer/Modules/Companies.js
+++ WebApiServer/Modules/Companies.js
@@ -9,5 +9,41 @@ users.controller("CompaniesController", function ($scope, $http, address) {
 })
     .error(function (data, status, headers, config) {
         alert('error');
-    })
+    });
+    $scope.submit_put = function ($company) {
+        alert(address + '/' + $company.Id);
+        $http.put(address + '/' + $company.Id, $company)
+        .success(function (data, status, headers, config) {
+
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
+
+    $scope.NewCompany = { 'Id': 0, 'Name': ''};
+    $scope.submit_post = function () {
+        $http.post(address, $scope.NewCompany)
+        .success(function (data, status, headers, config) {
+            $scope.NewCompany.Id = data.Id;
+            $scope.Companies = $scope.Companies.concat([$scope.NewCompany]);
+            $scope.NewCompany = { 'Id': 0, 'Name': ''};
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
+
+    $scope.delete = function ($company) {
+        $http.delete(address + '/' + $company.Id)
+        .success(function (data, status, headers, config) {
+            var i = $scope.Companies.indexOf($company);
+            if (i > -1) {
+                $scope.Companies.splice(i, 1);
+            }
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
 });
--- WebApiServer/Modules/Users.js
+++ WebApiServer/Modules/Users.js
@@ -9,5 +9,41 @@ users.controller("UsersController", function ($scope, $http, address) {
 })
     .error(function (data, status, headers, config) {
         alert('error');
-    })
+    });
+    $scope.submit_put = function ($user) {
+        alert(address + '/' + $user.Id);
+        $http.put(address + '/' + $user.Id, $user)
+        .success(function (data, status, headers, config) {
+            
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
+
+    $scope.NewUser = { 'Id': 0, 'Name': '', 'Login': '', 'Password': '', 'CompanyID': undefined, 'ContractStatus': undefined };
+    $scope.submit_post = function () {
+        $http.post(address, $scope.NewUser)
+        .success(function (data, status, headers, config) {
+            $scope.NewUser.Id = data.Id;
+            $scope.Users = $scope.Users.concat([$scope.NewUser]);
+            $scope.NewUser = { 'Id': 0, 'Name': '', 'Login': '', 'Password': '', 'CompanyID': undefined, 'ContractStatus': undefined };
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
+
+    $scope.delete = function ($user) {
+        $http.delete(address + '/' + $user.Id)
+        .success(function (data, status, headers, config) {
+            var i = $scope.Users.indexOf($user);
+            if (i > -1) {
+                $scope.Users.splice(i, 1);
+            }
+        })
+        .error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
 });
